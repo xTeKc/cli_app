936eeb6ddb2a9e99254424e854a5a4423fd58498
xTeKc <bcd3v@pm.me>
2021-11-27 21:19:57 -0500

    add run func

--- src/main.rs
+++ src/main.rs
@@ -20,3 +20,11 @@ fn main() {
         process::exit(1);
     }
 }
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("With text:\n{}", contents);
+
+    Ok(())
+}
