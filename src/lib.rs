70b52b74e8f44b3aae8eb0788829f36346a03243
xTeKc <bcd3v@pm.me>
2021-11-29 14:53:00 -0500

    mv to lib

--- src/lib.rs
+++ src/lib.rs
@@ -8,3 +8,20 @@ fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Ok(Config { query, filename })
+}
+}
