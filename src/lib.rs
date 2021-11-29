f0221b1c13bbfe36c525ebde49f1d9f63bd7fa41
xTeKc <bcd3v@pm.me>
2021-11-29 14:52:40 -0500

    mv to lib

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+
+
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("With text:\n{}", contents);
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -21,14 +21,6 @@ fn main() {
     }
 }
 
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
-
 struct Config {
     query: String,
     filename: String,
