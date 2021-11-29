fe0c72c25cb8f6f740708986fc9df04a5a84bc4f
xTeKc <bcd3v@pm.me>
2021-11-29 15:07:01 -0500

    setup init test

--- src/lib.rs
+++ src/lib.rs
@@ -25,3 +25,19 @@ impl Config {
     Ok(Config { query, filename })
 }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contetns = "\
+    Rust:
+    safe, fast, productive.
+    Pick three.";
+        
+        assert_eq!(vec!["safe, fast, productive."], search(query, contetns));
+    }
+}
